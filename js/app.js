1c434e365dd9699504d902c32b09572eb4966dc3
RBohara <rahul.bohara2016@gmail.com>
2018-04-09 01:22:34 +1000

    chnages in the html pages and css

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
-function previousImage() {
-    var image_count = document.getElementById('image-container').getElementsByTagName('img').length;
+function slideImage(){
     
-    if (image_count == 1){
+    var img1 = new Image();
+    img1.src = "img/alice-in-wonderland.jpg";
+
+    var img2 = new Image();
+    img2.src = "img/boromir.jpg";
+
+    var img3 = new Image();
+    img3.src = "img/book-fair.jpg";
+
+    var counter = 1;
+    
+    document.images.slide.src = eval("img"+counter+".src");
+    
+    if(counter<3){
+        counter = counter + 1;
+    }
+    else{
+        counter = 1;
         
     }
-    
+    setTimeout("slideImage()", 2500);
 }
+
+slideImage();
 
